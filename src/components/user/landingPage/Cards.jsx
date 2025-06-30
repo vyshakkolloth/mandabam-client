0e258fb98b9931ad7e41cb9e66bbcbf0a357afb7
vyshakkolloth <vyshakkolloth@gmail.com>
2025-06-30 22:48:59 +0530

    card design corection

--- src/components/user/landingPage/Cards.jsx
+++ src/components/user/landingPage/Cards.jsx
@@ -27,15 +27,16 @@ const Cards = () => {
             <div className="w-[150px]  h-[55px] sm:w-[110px] sm:h-[60px] ">
                 <img src="/images/Flower.png" alt="flower" className="w-full h-auto mx-auto" />
             </div>
-            <div className="flex flex-row   items-center justify-evenly flex-wrap gap-6 mt-4 ">
+            {/* <div className="flex flex-row   items-center justify-evenly sm:flex-nowrap flex-wrap gap-5 mt-4 "> */}
+            <div className="flex flex-row   items-center justify-around  flex-wrap gap-5 mt-4 ">
 
                 {data.map((values, index) => (
                     <Link to="/details" state={{ query: values._id }} key={index}>
                     
                     <div  className='max-w-sm flex flex-col items-center justify-center  glass p-4 rounded-lg border-2
                          border-red-100 hover:pointer:shadow-lg hover:border-red-200 transition-all duration-300 ease-in-out'>
-                        <img src={values.image[0]} alt="wedding" className="w-[15rem] h-[15rem] sm:w-[20rem] sm:h-[20rem] rounded-lg shadow-lg object-cover" />
-                        <div className='flex flex-col items-start justify-start text-left w-[15rem] sm:w-[20rem]  '>
+                        <img src={values.image[0]} alt="wedding" className="w-[15rem] h-[15rem] sm:max-w-[15rem] sm:max-h-[15rem] rounded-lg shadow-lg object-cover" />
+                        <div className='flex flex-col items-start justify-start text-left max-w-[15rem] sm:max-w-[15rem]  '>
                             <h4 className='text-gray-800 font-bold text-[1.2rem] mt-2'>{values.name}</h4>
                             <p className='text-gray-600 text-[.9rem] '>{values.location}</p>
                             <p className='text-gray-600 text-[.9rem] line-clamp-3 '>{values.information}.</p>
